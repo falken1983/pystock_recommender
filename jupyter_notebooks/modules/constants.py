2a6025c1b1c0410175f7ece4cc21ee07314e2203
Lluis Navarro <lluis2001@gmail.com>
2022-09-02 12:49:39 +0200

    1. Add features_construction jupyter notebooks: rolling sharpes (israelsen trick) and returns 2. Add modules helpers `constants.py` and `performance_measures_helper.py` 3. Add RankNET data transformations discussion `ranknet_data_layer_discussion`

---
+++ jupyter_notebooks/modules/constants.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+START_TRAINING_DATE_CUT="2012-06-29"
+END_TRAINING_DATE_CUT="2022-07-29"
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ jupyter_notebooks/modules/performance_measures_helper.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+
+""" 
+Performance Measures Related Functions
+"""
+#Sharpe Ratio Functions
+def sharpe_ratio(y, freq="daily"):
+    """Classical Sharpe Ratio. Annualized Ratio
+    """
+    scalers = {"daily": 252,
+               "monthly": 12,
+               "weekly": 50,
+               "biweekly": 25
+    }
+    # Annualized ratio
+    return np.sqrt(scalers[freq]) * (y.mean() / y.std()) 
+
+#TBD
+def israelsen_sharpe_ratio(y):    
+    """Israelsen Trick for the Sharpe Ratio
+    """
+    if y.mean()<0:
+        return np.sqrt(252) *(y.mean()*y.std())
+    else:
+        return sharpe_ratio(y)
+
+#Returns Related Functions
+def multi_period_return(period_returns):
+    return np.prod(period_returns + 1) - 1
+
+def net_cumreturn(data, last_row=False):
+    """Net Cumulative Returns
+    """
+    df = ((1+data).cumprod(axis=0)-1)
+    if last_row:
+        return df.iloc[-1]
+    return df
+
+""" 
+Scoring Related Functions
+"""
+def scorer(data, bins=20):
+    """ Simple Scorer (The Higher, the Better) """
+    df = pd.cut(x=data, bins=bins, labels=False)
+    return 1+df
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
